d8c2de579084a1aecaa4c3948e8fe202f25f0a41
leoninela <81444895+leonine2021@users.noreply.github.com>
2021-11-02 21:34:20 -0400

    update

--- pacmen.js
+++ pacmen.js
@@ -1,7 +1,7 @@
 let pos = 0;
 const pacArray = [
-  ['./images/PacMan1.png', './images/PacMan2.png'],
-  ['./images/PacMan3.png', './images/PacMan4.png'],
+  ["./images/PacMan1.png", "./images/PacMan2.png"],
+  ["./images/PacMan3.png", "./images/PacMan4.png"],
 ];
 let direction = 0;
 const pacMen = []; // This array holds all the pacmen
@@ -19,12 +19,13 @@ function makePac() {
   // returns an object with random values scaled {x: 33, y: 21}
   let velocity = setToRandom(10); // {x:?, y:?}
   let position = setToRandom(200);
+  let imgState = Math.round(Math.random());
 
   // Add image to div id = game
-  let game = document.getElementById('game');
-  let newimg = document.createElement('img');
-  newimg.style.position = 'absolute';
-  newimg.src = './images/PacMan1.png';
+  let game = document.getElementById("game");
+  let newimg = document.createElement("img");
+  newimg.style.position = "absolute";
+  newimg.src = pacArray[imgState][0];
   newimg.width = 100;
 
   // TODO: set position here
@@ -38,6 +39,7 @@ function makePac() {
   return {
     position,
     velocity,
+    imgState,
     newimg,
   };
 }
@@ -55,11 +57,43 @@ function update() {
   setTimeout(update, 20);
 }
 
+let counter = 0;
+function mouthMove() {
+  pacMen.forEach((item) => {
+    if (counter % 2 == 0) {
+      item.newimg.src = pacArray[item.imgState][0];
+    } else {
+      item.newimg.src = pacArray[item.imgState][1];
+    }
+  });
+  counter += 1;
+  setTimeout(mouthMove, 500);
+}
+
 function checkCollisions(item) {
   // TODO: detect collision with all walls and make pacman bounce
-  if(item.position.x+item.velocity.x+item.newimg.width > window.innerWidth || item.position.x+item.velocity.x <0) item.velocity.x = (-1)*item.velocity.x
+  if (
+    item.position.x + item.velocity.x + item.newimg.width > window.innerWidth ||
+    item.position.x + item.velocity.x < 0
+  ) {
+    changeDirectionIfNecessary(item);
+    item.velocity.x = -1 * item.velocity.x;
+  }
 
-  if(item.position.y+item.velocity.y+item.newimg.height > window.innerHeight || item.position.y+item.velocity.y < 0) item.velocity.y = (-1)*item.velocity.y
+  if (
+    item.position.y + item.velocity.y + item.newimg.height >
+      window.innerHeight ||
+    item.position.y + item.velocity.y < 0
+  )
+    item.velocity.y = -1 * item.velocity.y;
+}
+
+function changeDirectionIfNecessary(pacman) {
+  if (pacman.velocity.x > 0 && pacman.imgState == 0) {
+    pacman.imgState = 1;
+  } else if (pacman.velocity.x < 0 && pacman.imgState == 1) {
+    pacman.imgState = 0;
+  }
 }
 
 function makeOne() {
@@ -67,6 +101,6 @@ function makeOne() {
 }
 
 //don't change this line
-if (typeof module !== 'undefined') {
+if (typeof module !== "undefined") {
   module.exports = { checkCollisions, update, pacMen };
 }
